a0c3bec93b9d78a27f0a4f0ace7d41b3ea4a9374
Stuart Campbell <stuart@harto.org>
2020-12-07 22:44:56 -0800

    cleanup day 8

--- 08.py
+++ 08.py
@@ -1,26 +1,3 @@
-# with open('inputs/08') as f:
-#     prog = []
-#     for line in f:
-#         cmd, arg = line.strip().split(' ')
-#         prog.append((cmd, int(arg)))
-#
-# seen_lines = set()
-# acc = 0
-# i = 0
-# while i not in seen_lines:
-#     seen_lines.add(i)
-#     cmd, arg = prog[i]
-#     if cmd == 'acc':
-#         acc += arg
-#         i += 1
-#     elif cmd == 'jmp':
-#         i += arg
-#     elif cmd == 'nop':
-#         i += 1
-#     else:
-#         raise cmd
-# print(acc)
-
 with open('inputs/08') as f:
     orig_prog = []
     for line in f:
@@ -33,7 +10,7 @@ def run_prog(prog):
     i = 0
     while i < len(prog):
         if i in seen_lines:
-            return None
+            return acc, False
         seen_lines.add(i)
         cmd, arg = prog[i]
         if cmd == 'acc':
@@ -45,14 +22,17 @@ def run_prog(prog):
             i += 1
         else:
             raise cmd
-    return acc
+    return acc, True
+
+acc, _ = run_prog(orig_prog)
+print(acc)
 
 for i, line in enumerate(orig_prog):
     if line[0] not in ('jmp', 'nop'):
         continue
     modified_line = ('jmp' if line[0] == 'nop' else 'nop', line[1])
     modified_prog = orig_prog[:i] + [modified_line] + orig_prog[i+1:]
-    result = run_prog(modified_prog)
-    if result is not None:
-        print(result)
+    acc, finished = run_prog(modified_prog)
+    if finished:
+        print(acc)
         break
