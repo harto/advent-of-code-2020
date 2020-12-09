8775b4dbd70211408f782ceac2ce57a05a7f37a1
Stuart Campbell <stuart@harto.org>
2020-12-08 21:43:29 -0800

    09 cleaned up

--- 09.py
+++ 09.py
@@ -1,23 +1,14 @@
 with open('inputs/09') as f:
     nums = [int(line.strip()) for line in f]
 
-# window = nums[:25]
-
-# def valid(n):
-#     for i in range(len(window)):
-#         for j in range(1, len(window)):
-#             if window[i] + window[j] == n:
-#                 return True
-#     return False
-
-# for i in range(len(window), len(nums)):
-#     n = nums[i]
-#     if not valid(n):
-#         print(n)
-#         break
-#     window = window[1:] + [n]
-
-invalid = 552655238
+for i in range(25, len(nums)):
+    n = nums[i]
+    if not any(nums[j] + nums[k] == n
+               for j in range(i - 25, i)
+               for k in range(j + 1, i)):
+        invalid = n
+        print(n)
+        break
 
 i = 0
 j = 2
