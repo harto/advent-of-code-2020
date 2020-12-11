bf90e5da6370f815ec0f523747f37905cf937813
Stuart Campbell <stuart@harto.org>
2020-12-11 11:34:58 -0800

    11 cleaned up

--- 11.py
+++ 11.py
@@ -1,60 +1,61 @@
 def parse(s):
     return {(x, y): c == '#'
             for y, row in enumerate(s.strip().split('\n'))
-            for x, c in enumerate(row.strip())
+            for x, c in enumerate(row)
             if c != '.'}
 
-# def dump(state):
-#     for y in range(max(y for _, y in state) + 1):
-#         print(''.join(['.' if (x,y) not in state else '#' if state[(x,y)] else 'L'
-#                        for x in range(max(x for x, _ in state) + 1)]))
-
 with open('inputs/11') as f:
-    initial_state = parse(f.read().strip())
+    initial_seats = parse(f.read().strip())
 
-prev_state = None
-next_state = initial_state
+width = max(x for x, _ in initial_seats) + 1
+height = max(y for _, y in initial_seats) + 1
 
-# def occupied_neighbours(state, seat):
-#     x, y = seat
-#     neighbours = [(nx, ny) for nx in range(x-1, x+2)
-#                   for ny in range(y-1, y+2)
-#                   if not (nx == x and ny == y)]
-#     return sum(1 for pos in neighbours if pos in state and state[pos])
+def dump(seats):
+    return '\n'.join(''.join(['.' if (x, y) not in seats else '#' if seats[(x, y)] else 'L'
+                              for x in range(width)])
+                     for y in range(height))
 
+neighbour_offsets = [(x, y) for x in (-1, 0, 1) for y in (-1, 0, 1)
+                     if not (x == 0 and y == 0)]
 
-max_x = max(x for x, _ in initial_state.keys())
-max_y = max(y for _, y in initial_state.keys())
+# def occupied_neighbours(seats, pos):
+#     x, y = pos
+#     neighbours = [(x + dx, y + dy) for dx, dy in neighbour_offsets]
+#     return sum(1 for pos in neighbours if pos in seats and seats[pos])
+
 visible_from = {}
-for x, y in initial_state:
-    visible_from[(x, y)] = []
-    for dx in range(-1, 2):
-        for dy in range(-1, 2):
-            if dx == 0 and dy == 0: continue
-            nx, ny = x + dx, y + dy
-            while 0 <= nx <= max_x and 0 <= ny <= max_y:
-                if (nx, ny) in initial_state:
-                    visible_from[(x, y)].append((nx, ny))
-                    break
-                nx += dx
-                ny += dy
+for sx, sy in initial_seats:
+    visible_from[(sx, sy)] = []
+    for dx, dy in neighbour_offsets:
+        x = sx + dx
+        y = sy + dy
+        while 0 <= x < width and 0 <= y < height:
+            if (x, y) in initial_seats:
+                visible_from[(sx, sy)].append((x, y))
+                break
+            x += dx
+            y += dy
 
-def visibly_occupied_seats(state, seat):
-     return sum(1 for other_seat in visible_from[seat] if state[other_seat])
+def visibly_occupied(seats, pos):
+     return sum(1 for seen_pos in visible_from[pos] if seats[seen_pos])
 
-iterations = 0
-while prev_state != next_state:
-    prev_state = next_state
-    next_state = {}
-    # print('\niteration', iterations)
-    # dump(prev_state)
-    for seat, occupied in prev_state.items():
-        if occupied and visibly_occupied_seats(prev_state, seat) >= 5:
-            next_state[seat] = False
-        elif not occupied and visibly_occupied_seats(prev_state, seat) == 0:
-            next_state[seat] = True
-        else:
-            next_state[seat] = occupied
-    iterations += 1
+def should_occupy(seats, pos):
+    currently_occupied = seats[pos]
+    # occupied_others = occupied_neighbours(seats, pos)
+    # if currently_occupied and occupied_others >= 4:
+    occupied_others = visibly_occupied(seats, pos)
+    if currently_occupied and occupied_others >= 5:
+        return False
+    elif not currently_occupied and occupied_others == 0:
+        return True
+    else:
+        return currently_occupied
 
-print(sum(1 for occupied in next_state.values() if occupied))
+prev_seats = None
+curr_seats = initial_seats
+
+while prev_seats != curr_seats:
+    prev_seats = curr_seats
+    curr_seats = { pos: should_occupy(prev_seats, pos) for pos in prev_seats }
+
+print(sum(1 for occupied in curr_seats.values() if occupied))
