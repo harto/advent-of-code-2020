ef4c5464470c9fffc8b6522420b574c4d31de6ca
Stuart Campbell <stuart@harto.org>
2020-12-20 21:08:32 -0800

    19b

--- 19.py
+++ 19.py
@@ -9,26 +9,28 @@ for line in rule_text.split('\n'):
     if rest.startswith('"'):
         v = rest[1:2]
     else:
-        v = tuple(tuple(int(n) for n in alt.split(' ')) for alt in rest.split(' | '))
+        v = tuple(tuple(int(n) for n in option.split(' ')) for option in rest.split(' | '))
     rules[int(k)] = v
 
 def matches(rules, i, msg):
     rule = rules[i]
     if isinstance(rule, str):
-        return (True, msg[1:]) if (msg and msg[0] == rule) else False
-    for alt in rules[i]:
-        remaining = msg
-        match = True
-        for j in alt:
-            res = matches(rules, j, remaining)
-            if res:
-                remaining = res[1]
-            else:
-                match = False
-                break
-        if match:
-            return True, remaining
-    return False
+        if msg and msg[0] == rule:
+            return (msg[1:],)
+        else:
+            return ()
+    match_remainders = ()
+    for option in rule:
+        partial_matches = (msg,)
+        for j in option:
+            partial_matches = tuple(match for remaining in partial_matches
+                                    for match in matches(rules, j, remaining))
+        match_remainders += partial_matches
+    return match_remainders
+
+# part 2
+rules[8] = ((42,), (42, 8))
+rules[11] = ((42, 31), (42, 11, 31))
 
 print(len([match for match in (matches(rules, 0, msg) for msg in message_text.split('\n'))
-           if match and not match[1]]))
+           if '' in match]))
